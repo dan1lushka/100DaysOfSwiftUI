40000653f3f7af4831ba88f0d97c1063c0046526
Daniel Rybak <daniel.rybak91@gmail.com>
2019-12-15 01:00:57 +0000

    initial commit

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,8 +9,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    var cups = ["1","2","3","4","5","6","7","8","9","10+"]
+    
+    var bedTime: String {
+        let model = SleepCalculator()
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        
+        do {
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            
+            return formatter.string(from: sleepTime)
+        } catch {
+            return "Sorry, there was a problem calculating you bedtime."
+        }
+    }
+    
     var body: some View {
-        Text("Hello World")
+        
+        NavigationView {
+            
+            VStack {
+                
+                Form {
+                    Text("sleep at \(bedTime)")
+                        .font(.largeTitle)
+                }
+                
+                Form {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                
+                Section {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
+                    }
+                }
+                
+                Form {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Picker(selection: $coffeeAmount, label: Text("Coffee amount")) {
+                        ForEach(0 ..< cups.count) {
+                            Text("\(self.cups[$0]) cups")
+                        }
+                    }
+                    .labelsHidden()
+                }
+                    
+                .navigationBarTitle("BetterRest")
+            }
+        }
+    }
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
     }
 }
 
