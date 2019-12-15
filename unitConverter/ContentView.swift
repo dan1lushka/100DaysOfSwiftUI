c3e4a09bb56b80723713aad6978412cadb809f6b
Daniel Rybak <daniel.rybak91@gmail.com>
2019-12-15 00:27:45 +0000

    initial commit

--- unitConverter/ContentView.swift
+++ unitConverter/ContentView.swift
@@ -9,8 +9,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var id = 0
+    @State private var convertFrom = ""
+    @State private var convertTo = ""
+    @State private var amount = ""
+    
+    var unitSelections = ["meter","km","feet","yard","mile"]
+    
+    var conversionResult: Double {
+        let unitConversionDictioanry = ["meter": 1,
+                                        "km": 1000,
+                                        "feet": 0.3048,
+                                        "yard": 0.91439,
+                                        "mile": 1609.34]
+        let defaultConversionType = 1.00
+        let amountGiven = Double(amount) ?? 0
+        
+        let defaultFromValue = (unitConversionDictioanry[convertFrom] ?? 0) * defaultConversionType
+        let defaultToValue = (unitConversionDictioanry[convertTo] ?? 0) * defaultConversionType
+        
+        return (defaultFromValue / defaultToValue) * amountGiven
+    }
+    
     var body: some View {
-        Text("Hello World")
+        
+        NavigationView {
+            
+            Form {
+                
+                Section(header: Text("From")) {
+                    Picker("From", selection: $convertFrom ) {
+                        ForEach(0 ..< unitSelections.count) {
+                            Text("\(self.unitSelections[$0])")
+                                .tag(self.unitSelections[$0])
+                        }
+                    }
+                    .pickerStyle(DefaultPickerStyle())
+                    
+                    TextField("Enter amount", text: $amount)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section(header: Text("To")) {
+                    Picker("To", selection: $convertTo ) {
+                        ForEach(0 ..< unitSelections.count) {
+                            Text("\(self.unitSelections[$0])")
+                                .tag(self.unitSelections[$0])
+                        }
+                    }
+                    .pickerStyle(DefaultPickerStyle())
+                }
+                
+                Section(header: Text("Result")) {
+                    Text("\(conversionResult)")
+                }
+                
+            }
+            .navigationBarTitle("Length converter")
+        }
     }
 }
 
@@ -19,3 +75,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
